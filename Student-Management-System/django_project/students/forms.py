2abc4c1001816a471d2c54a6be53ba94e2b538d6
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2024-05-15 20:25:23 +0300

    Student-Management-System
    Developed with Django Framework

--- Student-Management-System/django_project/students/forms.py
+++ Student-Management-System/django_project/students/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Student
 
-class StudentForm(form.ModelForm):
+class StudentForm(forms.ModelForm):
     class Meta:
         model=Student
         fields=['student_number','first_name','last_name','email','field_of_study','gpa']
@@ -12,4 +12,12 @@ class StudentForm(form.ModelForm):
             'email':'Email',
             'field_of_study': 'Field of Study',
             'gpa': 'GPA'
+        }
+        widgets={
+            'student_number': forms.NumberInput(attrs={'class':'form_control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'field_of_study': forms.TextInput(attrs={'class':'form-control'}),
+            'gpa':forms.NumberInput(attrs={'class':'form-control'}),
         }
